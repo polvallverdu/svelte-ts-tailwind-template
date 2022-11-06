9b71de5a01438ce8d5a794a0d29b92013bcde930
Pol Vallverdu <pvallverdualcaide@gmail.com>
2022-11-06 20:10:28 +0100

    svelte kit done

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<div class="text-center">
+	<h1 class="text-7xl font-bold" style="">SvelteKit + TailwindCSS + Typescript template</h1>
+	<p>
+		<a href="https://github.com/polvallverdu/svelte-ts-tailwind-template/tree/svelte-kit" class="text-4xl text-blue-500 underline"
+			>Click here</a
+		>
+	</p>
+</div>
